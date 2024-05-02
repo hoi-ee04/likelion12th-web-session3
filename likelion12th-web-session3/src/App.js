329e9357c036972d062bbfb0f61f95980ebb8499
hoiee04 <chaehl1004@gmail.com>
2024-05-02 13:34:17 +0900

    다크모드 구현

--- likelion12th-web-session3/src/App.js
+++ likelion12th-web-session3/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Header from './components/Header';
 import Mylife from './components/Mylife';
 import Languages from './components/Languages';
@@ -6,12 +6,22 @@ import Link from './components/Link';
 import './App.css';
 
 function App() {
+  // 다크모드 상태를 관리하는 state
+  const [darkMode, setDarkMode] = useState(false);
+
+  // 다크모드 토글 함수
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+    // 다크모드 상태가 변경될 때 body 요소에 dark 클래스를 추가 또는 제거
+    document.body.classList.toggle('dark', !darkMode);
+  };
+
   return (
-    <div className="App">
-      <Header />
-      <Mylife />
-      <Languages />
-      <Link />
+    <div className={`App ${darkMode ? 'dark' : ''}`}>
+      <Header darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
+      <Mylife darkMode={darkMode} />
+      <Languages darkMode={darkMode} />
+      <Link darkMode={darkMode} />
     </div>
   );
 }
